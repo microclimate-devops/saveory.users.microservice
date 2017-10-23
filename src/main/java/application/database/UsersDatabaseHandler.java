e4261840627e8f0c1f18cf3a246271a4c07267ac
Daniel Schartner <daniel.schartner@ibm.com>
2017-10-23 16:17:50 -0400

    forgot to use ObjectId in all places

--- src/main/java/application/database/UsersDatabaseHandler.java
+++ src/main/java/application/database/UsersDatabaseHandler.java
@@ -82,7 +82,7 @@ public class UsersDatabaseHandler {
 	}
 
 	//Update database
-	getUsersCollection().replaceOne(eq("_id", token), userData);
+	getUsersCollection().replaceOne(new BasicDBObject("_id", new ObjectId(token)), userData);
 	return true;
    }
    
